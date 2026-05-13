61a5b2651f6d7a99cf4083492a228247e7b8a7ea
Arihant Singh <shubh610@gmail.com>
2026-05-13 10:34:45 -0400

    intital structure

---
+++ src/Shared/TenantPortal.Shared/Enums/TransactionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenantPortal.Shared.Enums
+{
+    public enum TransactionType
+    {
+        Rent, Deposit, LateFee, Maintenance, Utility, Parking, PetFee, Refund, Other
+    }
+}
---
+++ src/Shared/TenantPortal.Shared/Enums/UserRoles.cs
@@ -0,0 +1,7 @@
+namespace TentantPortal.Shared.Enums
+{
+    public enum UserRoles
+    {
+        SuperAdmin, Admin, Tenant, Tester
+    }
+}
